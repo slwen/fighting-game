94de264a879b41f1fa157533900c085c4af02881
Sam Enoka <slwenoka@gmail.com>
2023-04-25 14:40:32 +1000

    Set up player and enemy with animation

--- index.js
+++ index.js
@@ -6,20 +6,55 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+const gravity = .2
+
 class Sprite {
-  constructor(position) {
+  constructor({ position, velocity }) {
     this.position = position
+    this.velocity = velocity
+    this.height = 150
   }
 
   draw() {
     c.fillStyle = 'red'
-    c.fillRect(this.position.x, this.position.y, 50, 150)
+    c.fillRect(this.position.x, this.position.y, 50, this.height)
+  }
+
+  update() {
+    this.draw()
+    
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0
+    } else {
+      this.velocity.y += gravity
+      this.position.y += this.velocity.y
+    }
   }
 }
 
-const player = new Sprite({ x: 0, y: 0 })
-const enemy = new Sprite({ x: 400, y: 0 })
+const player = new Sprite(
+  {
+    position: { x: 0, y: 0 },
+    velocity: { x: 0, y: 0 }
+  }
+)
+const enemy = new Sprite(
+  {
+    position: { x: 400, y: 0 },
+    velocity: { x: 0, y: 0 }
+  }
+)
 
 player.draw()
 enemy.draw()
-console.log(player)
+console.log(player) // DEBUG
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  c.fillStyle = 'black'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  player.update()
+  enemy.update()
+}
+
+animate()
