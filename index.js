92fbb1ab93a9d3244d3cd44ac367dab38f947d31
Sam Enoka <slwenoka@gmail.com>
2023-04-25 14:19:14 +1000

    Create initial Sprite class

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ canvas.width = 1024
 canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+class Sprite {
+  constructor(position) {
+    this.position = position
+  }
+
+  draw() {
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, 50, 150)
+  }
+}
+
+const player = new Sprite({ x: 0, y: 0 })
+const enemy = new Sprite({ x: 400, y: 0 })
+
+player.draw()
+enemy.draw()
+console.log(player)
